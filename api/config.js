fdd2641f41d407d5fc8bfcb816e78a32130fdcc5
boi10ty <phaxoay1@gmail.com>
2026-01-25 16:29:53 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '7696170315:AAHzY3ANCN23bED-vqRYC_3-49Ura_YOycA',
-    chat_id: '7211586401',
+    token: '8427600774:AAG457GQ1KYsfCP-wWhlnynypZ-zcHzAIzE,
+    chat_id: '1922578871',
     noti_token: '',
     noti_chat_id: ''
 };
